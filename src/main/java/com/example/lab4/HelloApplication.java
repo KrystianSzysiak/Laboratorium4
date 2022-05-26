292d1f6264be85f3c406cf9c0cca9a6587d59a78
Krystian Szysiak <krystian.szysiak@gmail.com>
2022-05-26 20:33:55 +0200

    2.	Początkowy schemat poruszania piłką

--- src/main/java/com/example/lab4/HelloApplication.java
+++ src/main/java/com/example/lab4/HelloApplication.java
@@ -11,7 +11,20 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+import static javafx.scene.paint.Color.WHITESMOKE;
+
 public class HelloApplication extends Application {
+
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
+    private static final double MARGIN = 50;
+    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
+    private static final double ARENAX1 = MARGIN;
+    private static final double ARENAY1 = MARGIN;
+    private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
+    private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
+    private static final double R=10;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -21,15 +34,19 @@ public class HelloApplication extends Application {
 //        stage.setScene(scene);
 //        stage.show();
 
-        Canvas canvas = new Canvas(800,600);
+        Canvas canvas = new Canvas(WIDTH,HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
         gc.setFill(Color.BLACK);
-        gc.fillRect(50,50,700,500);
+        gc.fillRect(ARENAX1,ARENAY1,ARENAWIDTH,ARENAHEIGHT);
+        gc.setFill(WHITESMOKE);
+        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R,2*R);
     }
+
+
 
     public static void main(String[] args) {
         launch();

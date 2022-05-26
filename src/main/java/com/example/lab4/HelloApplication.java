61cd87248ecdeb2cf2e140c19931560a77739417
Krystian Szysiak <krystian.szysiak@gmail.com>
2022-05-26 21:36:32 +0200

    3.	Zderzenia elastyczne ze ścianami:

--- src/main/java/com/example/lab4/HelloApplication.java
+++ src/main/java/com/example/lab4/HelloApplication.java
@@ -1,6 +1,9 @@
 package com.example.lab4;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.NamedArg;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,7 +12,10 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.io.IOException;
+import java.util.EventListener;
 
 import static javafx.scene.paint.Color.WHITESMOKE;
 
@@ -25,6 +31,25 @@ public class HelloApplication extends Application {
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R=10;
+    private double x = ARENAX1+ARENAWIDTH/2;
+    private double y = ARENAY1+ARENAHEIGHT/2;
+    private double vx = 5;
+    private double vy = 2;
+
+    private void run(GraphicsContext gc)
+    {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1,ARENAY1,ARENAWIDTH,ARENAHEIGHT);
+
+        if((x<=ARENAX1) || ((x>=ARENAX2))) vx = -vx;
+        if((y<=ARENAY1) || ((y>=ARENAY2))) vy = -vy;
+
+        x +=vx;
+        y +=vy;
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(x,y,2*R,2*R);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -36,14 +61,14 @@ public class HelloApplication extends Application {
 
         Canvas canvas = new Canvas(WIDTH,HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1,ARENAY1,ARENAWIDTH,ARENAHEIGHT);
-        gc.setFill(WHITESMOKE);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R,2*R);
+        t.play();
     }
 
 
@@ -52,3 +77,4 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
